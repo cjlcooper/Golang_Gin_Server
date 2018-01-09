22311e1bcfb6c0883ccdcc820820311bb356ced7
cjlcooper <cjlcooper@163.com>
2018-01-09 17:28:13 +0800

    demo server

---
+++ Server01.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+  "fmt"
+  "github.com/gin-gonic/gin"
+  "net/http"
+  "database/sql"
+  _ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+    fmt.Println("Start Server ...")
+    InitDatabase()
+    CreateRouter()
+}
+
+//Create Router
+func CreateRouter() {
+    // set mode
+    gin.SetMode(gin.DebugMode)
+    // Register Middleware
+    router := gin.Default()
+    router.Use(Middleware)
+    // Test
+    router.GET("/test/get_test",GetTest)
+    router.POST("/test/post_test",PostTest)
+    // Listen Server
+    //http.ListenAndServe(":8080",router)
+    router.Run(":8080")
+}
+
+// Middle Ware
+func Middleware(c *gin.Context) {
+    fmt.Println("Start Middleware")
+}
+
+// Init Database
+func InitDatabase() {
+    db, err := sql.Open("mysql", "root:cjl1992@tcp(127.0.0.1:3306)/test?charset=utf8")
+    defer db.Close()
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    db.SetMaxIdleConns(20)
+    db.SetMaxOpenConns(20)
+
+    if err := db.Ping(); err != nil {
+        fmt.Println(err)
+    }
+}
+
+// Router Function
+func GetTest(c *gin.Context) {
+    value, exist := c.GetQuery("key")
+    if !exist {
+        value = "the key is not exist!"
+    }
+    c.Data(http.StatusOK, "text/plain", []byte(fmt.Sprintf("get success! %s\n", value)))
+    return
+}
+
+func PostTest(c *gin.Context) {
+    type JsonHolder struct {
+        Id   int    `json:"id"`
+        Name string `json:"name"`
+    }
+    holder := JsonHolder{Id: 1, Name: "my name"}
+    c.JSON(http.StatusOK, holder)
+    return
+}
+
+
